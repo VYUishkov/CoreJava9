d59c80f39197d8af64ba386e25e12e2b9ed82d3f
Ishkov Victor <VYuIshkov.SBT@sberbank.ru>
2017-05-19 16:40:05 +0300

    Статические методы. Для отработки использования статических методов был доработан класс Employee. Добавлено статическое поле nextId, c целью присвоения каждому создоному сотруднику регистрационного номера.

--- src/chapter4/EmployeeTest/Employee.java
+++ src/chapter4/EmployeeTest/Employee.java
@@ -4,9 +4,13 @@ import java.util.Date;
 import java.util.GregorianCalendar;
 
 public class Employee {
+    private static int nextId = 1;
+
     private String name;
     private double salary;
     private Date hireDay;
+
+    private int id;
 
     public Employee(String name, double salary, int year, int month, int day) {
         this.name = name;
@@ -14,30 +18,29 @@ public class Employee {
 
         GregorianCalendar calendar = new GregorianCalendar(year, month, day);
         hireDay = calendar.getTime();
+
+        setId();
     }
 
     public String getName() {
         return name;
     }
 
-    public void setName(String name) {
-        this.name = name;
-    }
-
     public double getSalary() {
         return salary;
-    }
-
-    public void setSalary(double salary) {
-        this.salary = salary;
     }
 
     public Date getHireDay() {
         return hireDay;
     }
 
-    public void setHireDay(Date hireDay) {
-        this.hireDay = hireDay;
+    public int getId() {
+        return id;
+    }
+
+    private void setId() {
+        id = nextId;
+        nextId++;
     }
 
     /**
